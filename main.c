8cced078e7478e37171ca90f4fb7adb4ce332f34
AdamKilani <142688635+AdamKilani@users.noreply.github.com>
2024-04-06 06:28:35 +0200

    Update main.c

--- main.c
+++ main.c
@@ -43,15 +43,10 @@ int main() {
         printf("\n");
 
         // Check for win condition
-if (blackPins == CODE_LENGTH) {
-    printf("Congratulations! You've guessed the code.\n");
-    break;
-}
-
-// Print the secret code for debugging purposes
-printf("Secret code: ");
-printColorString(secretCode, CODE_LENGTH);
-printf("\n");
+        if (blackPins == CODE_LENGTH) {
+            printf("Congratulations! You've guessed the code.\n");
+            break;
+        }
 
         attempts++;
     }
