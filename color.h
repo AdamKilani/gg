9cbae4895e536fb07c675ace0f9293bf510e22da
AdamKilani <142688635+AdamKilani@users.noreply.github.com>
2024-04-06 05:47:15 +0200

    Update color.h

--- color.h
+++ color.h
@@ -1,17 +1,21 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-// Colors enum
+// Déclarer l'énumération Color
 enum Color {
     RED,
     CYAN,
     YELLOW,
     GREEN,
     BLUE,
-    PURPLE
+    PURPLE,
+    UNKNOWN // Ajouter une valeur pour les cas non reconnus
 };
 
-// Function prototypes
+// Déclarer la fonction charToColor
+enum Color charToColor(char c);
+
+// Déclarer la fonction printColor
 void printColor(enum Color color);
 
-#endif /* COLOR_H */
+#endif
