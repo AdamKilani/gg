3105bc3e37df32d4de47b5c18ef7ebedb1f90edc
AdamKilani <142688635+AdamKilani@users.noreply.github.com>
2024-04-06 04:14:19 +0200

    Create color.h

---
+++ color.h
@@ -0,0 +1,17 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+// Colors enum
+enum Color {
+    RED,
+    CYAN,
+    YELLOW,
+    GREEN,
+    BLUE,
+    PURPLE
+};
+
+// Function prototypes
+void printColor(enum Color color);
+
+#endif /* COLOR_H */
